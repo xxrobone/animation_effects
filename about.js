f3d0b61e389b16ecc87413f4649e4014216ed0d6
robonexx <robertwagar@gmail.com>
2022-12-30 00:35:42 +0100

    Created a curtain opening intro screen animations with anime.js, too many ideas

--- about.js
+++ about.js
@@ -1,9 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const openCurtain = document.querySelector('.open_curtain');
+  const curtainTimeline = anime.timeline({
+    targets: '.open_curtain',
+    duration: 1000,
+    easing: 'easeOutSine',
+  });
+  curtainTimeline
+    .add({
+      targets: '.loading',
+      opacity: [0, 1],
+      width: ['0', '30vw'],
+      duration: 1400,
+      delay: 0,
+      innerHTML: [0, 100 + '%'],
+      round: 10,
+    })
+    .add({
+      targets: '.loading',
+      opacity: [1, 0],
+      height: ['50px', '0'],
+      duration: 300,
+      delay: 200,
+      innerHTML: [100, 0 + '%'],
+      round: 10,
+    })
+    .add({
+      targets: ['.left', '.right'],
+      opacity: 1,
+      width: ['50vw', '0vw'],
+      duration: 1400,
+      delay: 600,
+    })
+    .add({
+      delay: 400,
+      translateY: '-100vh',
+      duration: 1000,
+      complete: function () {
+        openCurtain.remove();
+      },
+    });
+});
+
 const btn = document.querySelector('.btn');
 
 // to get lenght of the svg path
-var ze = document.querySelector('.ze');
+/* var ze = document.querySelector('.ze');
 var length = ze.getTotalLength();
-console.log(length);
+console.log(length); */
 
 let toggle = false;
 
@@ -14,11 +57,10 @@ const letter2 =
 btn.addEventListener('click', () => {
   console.log('clicked');
 
-  ze.style.stroke = toggle ? '#28282880' : '#121212';
-
   const timeline = anime.timeline({
-    duration: 300,
+    targets: '.section',
     easing: 'easeOutSine',
+    duration: 500,
   });
   // add animation morph
   timeline
@@ -36,18 +78,7 @@ btn.addEventListener('click', () => {
         backgroundColor: toggle ? 'white' : 'black',
       },
       '-=200'
-    )
-    .add({
-      targets: '.ze',
-      strokeDashoffset: toggle
-        ? [anime.setDashoffset, 0]
-        : [anime.setDashoffset, -2145],
-      easing: 'easeInOutSine',
-      duration: 2000,
-      delay: 0.1,
-      direction: 'alternate',
-      loop: false,
-    });
+    );
 
   if (!toggle) {
     toggle = true;
