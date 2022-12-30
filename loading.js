5c68bed141b3bec603ade9a6f1cf6fcd847d203c
robonexx <robertwagar@gmail.com>
2022-12-30 21:49:32 +0100

    well, hmm. this loading , coffee thingy was not easy to get to look nice on mobile

--- loading.js
+++ loading.js
@@ -38,3 +38,41 @@ anime({
   translateY: [200, 0],
   rotate: [-45, 0],
 });
+
+textArr.forEach((a) => {
+  let textLineArr = a.textContent.split(' ');
+
+  console.log(textLineArr);
+
+  let html = '';
+
+  let animation = a.classList;
+  console.log('animation class list: ' + animation);
+
+  for (i = 0; i < textLineArr.length; ++i) {
+    html +=
+      " <span style='animation-delay:" +
+      i * 200 +
+      "ms; display:inline-block' class='" +
+      animation +
+      " animate'>" +
+      textLineArr[i] +
+      '</span>';
+
+    console.log(html);
+    a.innerHTML = html;
+
+    a.classList.remove('text_to_split');
+    console.log('after removal : ' + a.classList);
+  }
+
+  console.log('after loop ' + a.textContent);
+});
+
+anime({
+  targets: '.loading .hightlight',
+  easing: 'easeInOutSine',
+  duration: 1000,
+  delay: 4500,
+  translateY: [0, 10, 0, 5],
+});
