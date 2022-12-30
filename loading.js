ffc7a3a6859647774acfb44a67f56e20a6eb67f0
robonexx <robertwagar@gmail.com>
2022-12-30 20:38:50 +0100

    coffee waves animation, text path animation and a drop of milk ? eh

--- loading.js
+++ loading.js
@@ -1,5 +1,9 @@
 let textArr = Array.from(document.querySelectorAll('.text_to_split'));
+let logoLetters = Array.from(document.querySelectorAll('.logo_svg > path'));
 
+logoLetters.forEach((l, idx) => {
+  console.log(`this letter nr ${idx} = ${l.getTotalLength()}`);
+});
 /* A piece of sugar floating in coffee, is the idea... lol */
 var path = anime.path('.motion-path-demo path');
 
@@ -9,6 +13,28 @@ anime({
   translateY: path('y'),
   rotate: path('angle'),
   easing: 'linear',
-  duration: 10000,
-  loop: true,
+  duration: 7000,
+  loop: false,
+});
+
+anime({
+  targets: logoLetters,
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1000,
+  delay: anime.stagger(300),
+  direction: 'alternate',
+  loop: false,
+});
+
+anime({
+  targets: '.drop',
+  easing: 'easeInOutSine',
+  duration: 1000,
+  delay: 4500,
+  direction: 'forwards',
+  opacity: [0, 1],
+  translateX: [-200, 0],
+  translateY: [200, 0],
+  rotate: [-45, 0],
 });
